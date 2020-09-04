e37b0d1b348fdd191bc82faf551e7cbd33095854
Evan Mitchell <etmitchell@bsu.edu>
2020-09-04 18:39:36 -0400

    Final commit

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,60 @@
 def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+
+    roman_numerals = {
+        1000 => "M",
+         900 => "CM",
+         500 => "D",
+         400 => "CD",
+         100 => "C",
+          90 => "XC",
+          50 => "L",
+          40 => "XL",
+          10 => "X",
+           9 => "IX",
+           5 => "V",
+           4 => "IV",
+           1 => "I",
+    }
+if romanNumber != roman_numerals
+    raise TypeError
+end
+ 
+finalNumber = 0
+i = 0
+while i < romanNumber.length
+    if i + 1 < romanNumber.length && roman_numerals[romanNumber[i]] < roman_numerals[romanNumber[i + 1]]
+        finalNumber += roman_numerals[romanNumber[i + 1]] - hash[romanNumber[i]]
+        i += 1
+    else
+        finalNumber += roman_numerals[romanNumber[i]]
+    end
+    i += 1
+end
+finalNumber
 end
 
 def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
-end
+    raise RangeError unless arabicNumber >= 1 && arabicNumber <= 3999
+
+    roman_numerals = {
+        1000 => "M",
+         900 => "CM",
+         500 => "D",
+         400 => "CD",
+         100 => "C",
+          90 => "XC",
+          50 => "L",
+          40 => "XL",
+          10 => "X",
+           9 => "IX",
+           5 => "V",
+           4 => "IV",
+           1 => "I",
+    }
+    roman = ''
+    roman_numerals.each do |(letter, key)|
+        roman << key * (arabicNumber/letter)
+        arabicNumber = arabicNumber % letter
+    end
+    return roman
+  end
